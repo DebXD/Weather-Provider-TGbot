ed59bb156a131140ecf64ae87b0cb7213be2c376
Debiprasad Das <debiprasadxd@protonmail.com>
2024-01-13 19:39:49 +0530

    use axiod from deno instead of axios

--- apis/getCityLocation.ts
+++ apis/getCityLocation.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axiod from "https://deno.land/x/axiod/mod.ts";
 
 const getLocationData = async (cityName: string) => {
-  const res = await axios.get(
+  const res = await axiod.get(
     `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&language=en&format=json`,
   );
   return res.data.results[0];
--- apis/getWeatherData.ts
+++ apis/getWeatherData.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axiod from "https://deno.land/x/axiod/mod.ts";
 
 const getWeatherData = async (lat: string, lon: string, timezone: string) => {
   const url =
     `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,daylight_duration,sunshine_duration,precipitation_sum,wind_speed_10m_max&timezone=${timezone}&forecast_days=1`;
-  const response = await axios.get(url);
+  const response = await axiod.get(url);
   return response.data;
 };
 
